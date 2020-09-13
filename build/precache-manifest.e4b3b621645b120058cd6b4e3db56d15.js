1c601b068c29166edeb299d7fcfedbbceb981658
Taloqq <antte.alatalo@hotmail.com>
2020-09-13 10:18:03 +0300

    stack

---
+++ build/precache-manifest.e4b3b621645b120058cd6b4e3db56d15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cb1a14edec0246f021736393a7d7f68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "391f98452e40af86119f",
+    "url": "/static/css/main.76eac5ef.chunk.css"
+  },
+  {
+    "revision": "c833f9d86cf79ff4355b",
+    "url": "/static/js/2.3a567198.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3a567198.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "391f98452e40af86119f",
+    "url": "/static/js/main.9e822e0e.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
---
+++ build/precache-manifest.e71c769deb2c464be248daeb817399fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47475266656a183d9bb7a8be391962fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08c8a2e437729b860bff",
+    "url": "/static/css/main.76eac5ef.chunk.css"
+  },
+  {
+    "revision": "c833f9d86cf79ff4355b",
+    "url": "/static/js/2.3a567198.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3a567198.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08c8a2e437729b860bff",
+    "url": "/static/js/main.eedb0f3d.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
