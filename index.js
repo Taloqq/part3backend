af187c4929b959d5a9ac6167eadeaa1dd9f9b0bc
Taloqq <antte.alatalo@hotmail.com>
2020-09-16 14:39:22 +0300

    osa3 backend

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const Person = require('./models/person')
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
+// eslint-disable-next-line no-unused-vars
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
@@ -34,12 +35,13 @@ app.get('/api/persons/:id', (request, response, next) => {
       } else {
         response.status(404).end
       }
-  })
-  .catch(error => next(error))
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
+    // eslint-disable-next-line no-unused-vars
     .then(result => {
       response.status(204).end()
     })
@@ -95,7 +97,7 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
-    return response.status(400).json({error: error.message})
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
@@ -104,6 +106,7 @@ const errorHandler = (error, request, response, next) => {
 app.use(errorHandler)
 
 
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
--- models/person.js
+++ models/person.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
-var uniqueValidator = require('mongoose-unique-validator');
+var uniqueValidator = require('mongoose-unique-validator')
 mongoose.set('useFindAndModify', false)
-mongoose.set('useCreateIndex', true);
+mongoose.set('useCreateIndex', true)
 
 const url = process.env.MONGODB_URI
 
@@ -15,8 +15,8 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   })
 
 const personSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true, minlength: 3},
-  number: { type: String, required: true, minlength: 8}
+  name: { type: String, required: true, unique: true, minlength: 3 },
+  number: { type: String, required: true, minlength: 8 }
 })
 personSchema.plugin(uniqueValidator)
 personSchema.set('toJSON', {
